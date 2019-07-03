0906eef3f0d731c46c001b1bbe5e06eab8eaa9ce
Yemre <aybey77@gmail.com>
2019-07-03 16:36:58 +0300

    user model

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get signup_path
+    get  get signup_path
     assert_response :success
   end
 
